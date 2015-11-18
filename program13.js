a0ff692ecb95187b17ee12a1f98b9d5088ff4613
Vincent Song <songvc@users.noreply.github.com>
2015-11-18 11:55:29 -0800

    learnyounode done

---
+++ program13.js
@@ -0,0 +1,38 @@
+var http = require('http'),
+	url = require('url'),
+	port = process.argv[2];
+
+var server = http.createServer(function(request,response){
+	
+	var path = url.parse(request.url, true).pathname;
+	var q = url.parse(request.url, true).query.iso;
+
+	var date = new Date(q);
+	var unix = date.getTime();
+
+	var J = {
+		"hour" 		: date.getHours(),
+		"minute"	: date.getMinutes(),
+		"second"	: date.getSeconds()
+	}
+
+	var U = {
+		"unixtime"	: unix
+	}
+
+
+	if (path == "/api/parsetime"){
+		
+		response.writeHead(200, {'Content-Type': 'application/json'});			
+		response.end(JSON.stringify(J));
+
+	}else if (path == "/api/unixtime"){
+		response.writeHead(200, {'Content-Type': 'application/json'});
+		response.end(JSON.stringify(U));
+
+	}
+
+
+});
+
+server.listen(port);
